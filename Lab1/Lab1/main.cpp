ded3d9c7ae39577e20238376676a05acd46a2c5d
noahlattari <zennoah1998@gmail.com>
2019-02-04 20:18:54 -0500

    selection sort

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -8,36 +8,60 @@
 #include <vector>
 
 int SIZE = 10;
-std::vector<int> arr(SIZE);
+std::vector<int> ARR(SIZE);
 
 
 void fillArray();
-void printUnsorted();
+void printUnsorted(std::vector<int>array);
+std::vector<int> selectionSort(std::vector<int>arr, int n);
+
+
 int main(int argc, const char * argv[]) {
-
+    
     fillArray();
-    printUnsorted();
+    ARR = selectionSort(ARR, SIZE);
+    printUnsorted(ARR);
+    
+    SIZE = 100;
+    fillArray();
+    ARR = selectionSort(ARR, SIZE);
+    printUnsorted(ARR);
+    //mergesort()
+    
     return 0;
-    
-    
-    
+}
 
+std::vector<int> selectionSort(std::vector<int>arr, int n)
+{
+    int i, j, minIndex, tmp;
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        if (minIndex != i) {
+            tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+        }
+    }
+    return arr;
 }
 
 void fillArray()
 {
-    arr.resize(SIZE);
+    ARR.resize(SIZE);
     std::cout << "Size of array (n) is " << SIZE << std::endl;
     for(int i = 0; i < SIZE; i++)
     {
-        arr[i] = rand() % (4 * SIZE) + 1;
+        ARR[i] = rand() % (4 * SIZE) + 1;
     }
 }
 
-void printUnsorted()
+void printUnsorted(std::vector<int>array)
 {
     for(int i = 0; i < SIZE; i++)
     {
-        std::cout << arr[i] << std::endl;
+        std::cout << array[i] << std::endl;
     }
 }
