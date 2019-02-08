f0661aca65303cf2df4f07e57a819ccfeb445ff4
noahlattari <zennoah1998@gmail.com>
2019-02-07 22:20:27 -0500

    fixed merge sort

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -21,80 +21,229 @@ std::vector<int> selectionSort(std::vector<int>arr, int n);
 std::vector<int> test(std::vector<int>arr);
 std::vector<int> mergeSort(std::vector<int> unsorted);
 std::vector<int> merge(std::vector<int>left, std::vector<int>right);
+std::vector<int> mergeSelect(std::vector<int>left, std::vector<int>right);
+std::vector<int> mergeAndSelect(std::vector<int>unsorted);
 
 int main(int argc, const char * argv[]) {
-    /*
+    
+    
+    
     cout << "Selection Sort\n" << endl;
-    ARR = fillArray(ARR);
+    
+    ARRAY = fillArray(ARRAY);
     auto start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
+    ARRAY = selectionSort(ARRAY, SIZE);
     auto stop = high_resolution_clock::now();
-    printUnsorted(ARR);
+    printUnsorted(ARRAY);
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
-    /*
+    
     SIZE = 100;
-    //fillArray();
+    ARRAY = fillArray(ARRAY);
     start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
+    ARRAY = selectionSort(ARRAY, SIZE);
     stop = high_resolution_clock::now();
-    //printUnsorted(ARR);
+    //printUnsorted(ARRAY);
     duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
     SIZE = 1000;
-   // fillArray();
+    ARRAY = fillArray(ARRAY);
     start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
+    ARRAY = selectionSort(ARRAY, SIZE);
     stop = high_resolution_clock::now();
-    //printUnsorted(ARR);
+    //printUnsorted(ARRAY);
     duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
     SIZE = 10000;
-    //fillArray();
+    ARRAY = fillArray(ARRAY);
     start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
+    ARRAY = selectionSort(ARRAY, SIZE);
     stop = high_resolution_clock::now();
-    //printUnsorted(ARR);
+    //printUnsorted(ARRAY);
     duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
-    /*
-    SIZE = 100000;
-    fillArray();
-    start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
-    stop = high_resolution_clock::now();
-    //printUnsorted(ARR);
-    duration = duration_cast<microseconds>(stop - start);
-    cout << "Time taken by function: of size 5 " << SIZE << "\n" << duration.count() << " microseconds" << endl;
-    */
-    
-    /*
-    SIZE = 1000000;
-    fillArray();
-    start = high_resolution_clock::now();
-    ARR = selectionSort(ARR, SIZE);
-    stop = high_resolution_clock::now();
-    //printUnsorted(ARR);
-    duration = duration_cast<microseconds>(stop - start);
-    cout << "Time taken by function: of size 6" << SIZE << "\n" << duration.count() << " microseconds" << endl;
-    */
-   
- 
-    SIZE = 10;
-    
+    SIZE = 20000;
     ARRAY = fillArray(ARRAY);
-    printUnsorted(ARRAY);
-    cout << "merging" << endl;
+    start = high_resolution_clock::now();
+    ARRAY = selectionSort(ARRAY, SIZE);
+    stop = high_resolution_clock::now();
+    //printUnsorted(ARRAY);
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 30000;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = selectionSort(ARRAY, SIZE);
+    stop = high_resolution_clock::now();
+    //printUnsorted(ARRAY);
+    duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    cout << "Merge Sort" << endl;
+    
+    SIZE = 10;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
     ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
     printUnsorted(ARRAY);
-    cout << "Done" << endl;
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
+    SIZE = 100;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
+    SIZE = 1000;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+   // printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     
+    SIZE = 10000;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+   // printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 20000;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+   // printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 100000;
+    ARRAY = fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeSort(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    cout << "Merge with Selection Sort" << endl;
+    
+    SIZE = 10;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 100;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 1000;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 10000;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 20000;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
+    
+    SIZE = 100000;
+    ARRAY=fillArray(ARRAY);
+    start = high_resolution_clock::now();
+    ARRAY = mergeAndSelect(ARRAY);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start);
+    //printUnsorted(ARRAY);
+    cout << "Time taken by function: of size " << SIZE << "\n" << duration.count() << " microseconds" << endl;
     return 0;
+}
+
+std::vector<int> mergeAndSelect(std::vector<int>unsorted){
+    if(unsorted.size() == 1){
+        return unsorted;
+    }
+    
+    std::vector<int>left;
+    std::vector<int>right;
+    std::copy(std::begin(unsorted), std::begin(unsorted) + unsorted.size()/2, std::back_inserter(left));
+    std::copy(std::begin(unsorted) + unsorted.size()/2, std::end(unsorted), std::back_inserter(right));
+    
+    
+    int m = SIZE+1/2;
+    
+    if(m == 7){
+       return unsorted = selectionSort(unsorted, SIZE);
+    } else{
+        left = mergeAndSelect(left);
+        right = mergeAndSelect(right);
+    }
+    return mergeSelect(left, right);
+}
+
+std::vector<int> mergeSelect(std::vector<int>left, std::vector<int>right){
+    std::vector<int>temp;
+    int l = 0, r = 0;
+    
+    while (l < left.size() && r < right.size()) {
+        if (left[l] > right[r]) {
+            temp.push_back(right[r]);
+            r++;
+        } else {
+            temp.push_back(left[l]);
+            l++;
+        }
+    }
+    
+    while(l < left.size()){
+        temp.push_back(left[l]);
+        l++;
+    }
+    
+    while(r < right.size()){
+        temp.push_back(right[r]);
+        r++;
+    }
+    
+    return temp;
 }
 
 std::vector<int> merge(std::vector<int>left, std::vector<int>right)
@@ -165,11 +314,11 @@ std::vector<int> selectionSort(std::vector<int>arr, int n)
 
 std::vector<int> fillArray(std::vector<int>array)
 {
-    array.resize(array.size());
+    array.resize(SIZE);
     std::cout << "Size of array (n) is " << SIZE << std::endl;
-    for(int i = 0; i < array.size(); i++)
+    for(int i = 0; i < SIZE; i++)
     {
-        array[i] = rand() % (4 * array.size()) + 1;
+        array[i] = rand() % (4 * SIZE) + 1;
     }
     
     return array;
@@ -179,6 +328,6 @@ void printUnsorted(std::vector<int>array)
 {
     for(int i = 0; i < array.size(); i++)
     {
-        std::cout << array[i] << std::endl-;
+        std::cout << array[i] << std::endl;
     }
 }
---
+++ squareSorted.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        System.out.println("Test");
+    }
+}
