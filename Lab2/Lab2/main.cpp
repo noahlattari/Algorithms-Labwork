820ed6777ddd03d6d4be2581e6269532327211a5
noahlattari <zennoah1998@gmail.com>
2019-02-08 11:22:11 -0500

    test

---
+++ Lab2/Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Noah Lattari on 2019-02-08.
+//  Copyright © 2019 Noah Lattari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
