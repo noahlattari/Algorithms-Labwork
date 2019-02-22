2c3a76ba88515a5308fd6656bf9221570fcd13c1
noahlattari <zennoah1998@gmail.com>
2019-02-21 19:51:03 -0500

    added lab3

---
+++ Lab3/Lab3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Noah Lattari on 2019-02-21.
+//  Copyright © 2019 Noah Lattari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
