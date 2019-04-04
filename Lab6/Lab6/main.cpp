2496097332551d376511554651eb23d31521151b
noahlattari <zennoah1998@gmail.com>
2019-04-04 12:18:31 -0400

    started lab6

---
+++ Lab6/Lab6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Noah Lattari on 2019-04-04.
+//  Copyright © 2019 Noah Lattari. All rights reserved.
+//
+
+#include <iostream>
+
+    int main(int argc, const char * argv[]) {
+        int value[] = {210, 220, 180, 120, 160, 170, 90, 40, 60, 10};
+        int weight[] = { {}
+        return 0;
+    }
